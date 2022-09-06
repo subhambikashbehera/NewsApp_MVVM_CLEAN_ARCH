efcc64bd93147c59f4a88a10d7757f6052fed9a4
@subham.behera2 <subham.behera@iserveu.in>
2022-09-06 15:41:50 +0530

    merging

--- app/src/main/java/com/onlineexam/newsapp/presenter/HomeActivity.kt
+++ app/src/main/java/com/onlineexam/newsapp/presenter/HomeActivity.kt
@@ -1,13 +1,13 @@
 package com.onlineexam.newsapp.presenter
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.onlineexam.newsapp.R
 import com.onlineexam.newsapp.databinding.ActivityMainBinding
@@ -22,30 +22,79 @@ import javax.inject.Inject
 class HomeActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModelFactory:LiveNewsViewModelFactory
+    lateinit var viewModelFactory: LiveNewsViewModelFactory
 
-    lateinit var viewModel:LiveNewsViewModel
+    lateinit var viewModel: LiveNewsViewModel
 
     @Inject
     lateinit var newsAdapter: NewsAdapter
 
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         supportActionBar?.hide()
 
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
         val navController = navHostFragment.navController
-//        val appBarConfiguration= AppBarConfiguration(setOf(R.id.newsFragment,R.id.savedNewsFragment))
-//        setupActionBarWithNavController(navController,appBarConfiguration)
         binding.bottomNav.setupWithNavController(navController)
-        viewModel= ViewModelProvider(this,viewModelFactory)[LiveNewsViewModel::class.java]
+        viewModel = ViewModelProvider(this, viewModelFactory)[LiveNewsViewModel::class.java]
+
+ /*       binding.bottomNav.setOnNavigationItemSelectedListener { it ->
+            when (it.itemId) {
+                R.id.savedNewsFragment -> {
+                    navController.navigate(R.id.savedNewsFragment)
+                    true
+                }
+                R.id.newsFragment -> {
+
+
+                    navController.navigate(R.id.newsFragment)
+                    true
+                }
+
+                R.id.extraFragment -> {
+                    Toast.makeText(this, "features not available", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                  false
+                }
+            }
+        }
+
+*/
+
+        binding.bottomNav.setOnItemSelectedListener {
+            if (NavigationUI.onNavDestinationSelected(it, navController)) {
+                true
+            } else {
+                when (it.itemId) {
+                    R.id.savedNewsFragment -> {
+                        navController.navigate(R.id.savedNewsFragment)
+                        true
+                    }
+                    R.id.newsFragment -> {
+                        navController.navigate(R.id.newsFragment)
+                        true
+                    }
+
+                    R.id.extraFragment -> {
+                        Toast.makeText(this, "features not available", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> {
+                        false
+                    }
+                }
+            }
+        }
+
 
     }
-
 
 
 }
